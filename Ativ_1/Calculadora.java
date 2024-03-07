da4d038ec8fca97307e4b0c5e5473d6d38a13003
wyllmayah <94290063+wildsonmaia@users.noreply.github.com>
2024-03-07 09:44:30 -0300

    Atividades 1 e 2

---
+++ Ativ_1/Calculadora.java
@@ -0,0 +1,23 @@
+package Atividades.Ativ_1;
+public class Calculadora {
+    
+    public double soma (double n1, double n2) {
+        double resultado = n1 + n2;
+        return resultado;
+    }
+
+    public double subt (double n1, double n2) {
+        double resultado = n1 - n2;
+        return resultado;
+    }
+
+    public double mult (double n1, double n2) {
+        double resultado = n1 * n2;
+        return resultado;
+    }
+
+    public double div (double n1, double n2) {
+        double resultado = n1 / n2;
+        return resultado;
+    }
+}
